3c0e36ffdf16c75bdde2e39f8fd7570a4234dc84
Anton-XIX <anton_kiryako2@mail.ru>
2021-03-03 10:21:06 +0300

    Changed url for hometask

--- blog/urls.py
+++ blog/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('product/update/<str:pk>',ProductUpdateView.as_view()),
     path('product/list',ProductListView.as_view()),
     path('product/search/',NameSearchProductAPI.as_view()),
-    path('product/name/<int:pk>',ProductNamePatchView.as_view())
+    path('product/update/name/<int:pk>',ProductNamePatchView.as_view())
 
 ]
